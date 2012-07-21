d16d29004e84aecc254d396ad56eb329cfd857e6
Hongze Zhao <srzhz@hotmail.com>
2012-07-21 14:56:46 +0800

    add some materials

--- src/photonMapping/materials/IMaterial.java
+++ src/photonMapping/materials/IMaterial.java
@@ -1,5 +1,20 @@
 package photonMapping.materials;
 
+import photonMapping.Color;
+
 public interface IMaterial {
+	double getReflection();
+
+	/**
+	 * the ratio of refraction
+	 * 
+	 * @return [0, 1]
+	 * @author Hongze Zhao
+	 */
+	double getRefraction();
+
+	double getRefractivity();
+
+	Color getColor();
 
 }
