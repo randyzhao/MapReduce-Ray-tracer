a55de4b5be14934d1b5f439a66ba7a3782f0ac0b
Hongze Zhao <srzhz@hotmail.com>
2012-07-22 13:32:09 +0800

    add default scene

--- src/photonMapping/scene/Scene.java
+++ src/photonMapping/scene/Scene.java
@@ -17,8 +17,13 @@ package photonMapping.scene;
 import java.util.ArrayList;
 import java.util.List;
 
+import photonMapping.Color;
+import photonMapping.Vector;
+import photonMapping.lightSource.DotLightSource;
 import photonMapping.lightSource.ILightSource;
+import photonMapping.materials.DiffuseMaterial;
 import photonMapping.shape.IShape;
+import photonMapping.shape.Sphere;
 
 /**
  *
@@ -46,6 +51,15 @@ public class Scene {
 
 	public void setShapes(List<IShape> shapes) {
 		this.shapes = shapes;
+	}
+
+	public static Scene testScene() {
+		Scene scene = new Scene();
+		scene.shapes.add(new Sphere(5, new Vector(0, 0, 0),
+				new DiffuseMaterial(0.5, new Color(1, 0, 0))));
+		scene.lightSource = new DotLightSource(new Vector(5, 5, 5), new Color(
+				0, 1, 0));
+		return scene;
 	}
 
 }
---
+++ src/photonMapping/scene/XMLSceneBuilder.java
@@ -0,0 +1,25 @@
+/**  
+ * Filename:    XMLSceneBuilder.java  
+ * Description:   
+ * Copyright:   Copyright (c)2011 
+ * Company:    company 
+ * @author:     Hongze Zhao 
+ * @version:    1.0  
+ * Create at:   Jul 22, 2012 1:26:48 PM  
+ *  
+ * Modification History:  
+ * Date         Author      Version     Description  
+ * ------------------------------------------------------------------  
+ * Jul 22, 2012    Hongze Zhao   1.0         1.0 Version  
+ */
+package photonMapping.scene;
+
+
+/**
+ *
+ * @author Hongze Zhao
+ * Create At : Jul 22, 2012 1:26:48 PM
+ */
+public class XMLSceneBuilder {
+
+}
